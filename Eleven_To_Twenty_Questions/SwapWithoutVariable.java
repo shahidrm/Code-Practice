996844311cc2ea943f50abdfd2f19a7767bd393e
shahidr0909 <mdshahidraja0909@gmail.com>
2024-06-30 01:34:03 +0530

    Third Commit

--- Eleven_To_Twenty_Questions/SwapWithoutVariable.java
+++ Eleven_To_Twenty_Questions/SwapWithoutVariable.java
@@ -6,11 +6,10 @@ public class SwapWithoutVariable {
 	
 //	14. Write a Java program to swap two numbers without using a third variable.
 
-
 	public static void main(String[] args) {
 
 		int a,b;
-		System.out.println("Enter two Numbers");
+		System.out.println("Enter two Numbers ");
 		Scanner sc = new Scanner(System.in);
 		a = sc.nextInt();
 		b = sc.nextInt();
