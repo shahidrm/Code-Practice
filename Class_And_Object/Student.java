b1914b3cefe3fbae636471b243fc4a308cca9e9b
shahidr0909 <mdshahidraja0909@gmail.com>
2024-07-08 12:00:16 +0530

    First Commit

--- Class_And_Object/Student.java
+++ Class_And_Object/Student.java
@@ -2,9 +2,30 @@ package Class_And_Object;
 
 public class Student {
 
-	public static void main(String[] args) {
+	    private String name;
+	    private int age;
 
-		
-	}
+	    // Constructor
+	    public Student(String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
 
+	    // Getter for name
+	    public String getName() {
+	        return name;
+	    }
+
+	    // Getter for age
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public static void main(String[] args) {
+	    Student std = new Student("John Doe", 20);
+	    
+	    System.out.println("Student Name: " + std.getName());
+	    System.out.println("Student Age: " + std.getAge());
+	    
+	    }	
 }
