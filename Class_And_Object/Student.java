15f0bfe9a49d69e17c8ed02eebc6ec00afaef67e
shahidr0909 <mdshahidraja0909@gmail.com>
2024-07-08 11:52:09 +0530

    Interview Question

---
+++ Class_And_Object/Student.java
@@ -0,0 +1,10 @@
+package Class_And_Object;
+
+public class Student {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
---
+++ Collection/List.java
@@ -0,0 +1,10 @@
+package Collection;
+
+public class List {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
