6f850c552b8ac6bcd6cd4e63c00ee868c7fda105
shahidr0909 <mdshahidraja0909@gmail.com>
2024-07-10 20:26:10 +0530

    first commit

--- Class_And_Object/Employee.java
+++ Class_And_Object/Employee.java
@@ -1,10 +1,51 @@
 package Class_And_Object;
 
 public class Employee {
+	
+//	2. Create a class Employee with private fields name and salary. Provide public getter and setter methods for these fields.
+	
+	    private String name;
+	    private double salary;
+
+	    // Constructor
+	    public Employee(String name, double salary) {
+	        this.name = name;
+	        this.salary = salary;
+	    }
+
+	    // Getter setter for name
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    // Getter setter for salary
+	    public double getSalary() {
+	        return salary;
+	    }
+
+	    public void setSalary(double salary) {
+	        this.salary = salary;
+	    }
+
 
 	public static void main(String[] args) {
-
 		
-	}
+		// Object
+        Employee emp = new Employee("Md Shahid", 50000.0);
 
+        System.out.println("Employee Name: " + emp.getName());
+        System.out.println("Employee Salary: " + emp.getSalary());
+        
+//        if we want to update details then we need to used this process. 
+        emp.setName("Rajesh Kumar");
+        emp.setSalary(60000.0);
+
+        System.out.println("Updated Employee Name: " + emp.getName());
+        System.out.println("Updated Employee Salary: " + emp.getSalary());
+
+	}
 }
